2e16e7e8e7729291dcae62b0516d2a296b455f83
yamid-dev <yhrodriguez1@hotmail.com>
2024-02-17 13:09:27 -0500

    Modificacion del run de la aplicacion

--- marvyshopmarket/run.py
+++ marvyshopmarket/run.py
@@ -1,4 +1,8 @@
-from app import app
+from app import create_app
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Crear la aplicación Flask
+app = create_app()
+
+# Ejecutar la aplicación Flask
+if __name__ == '__main__':
+    app.run(debug=True)  
