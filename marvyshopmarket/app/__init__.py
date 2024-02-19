c76f4be050b26a3c100b36f0aedd50b819854928
yamid-dev <yhrodriguez1@hotmail.com>
2024-02-19 06:15:54 -0500

    Modifique configuración app

--- marvyshopmarket/app/__init__.py
+++ marvyshopmarket/app/__init__.py
@@ -7,7 +7,8 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:nidian56@localhost/marvy_shopmarket'
     app.config['SQLALCHEMY_BINDS'] = {
-        'image':'mysql://root:nidian56@localhost/images'
+        'image':'mysql://root:nidian56@localhost/images',
+        'producto': 'mysql://root:nidian56@localhost/marvy_shopmarket'
     }
     db.init_app(app)
     
