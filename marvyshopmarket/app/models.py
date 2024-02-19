52528fe4c40b540f392b23cadcd62f12bbc3fa5c
yamid-dev <yhrodriguez1@hotmail.com>
2024-02-19 06:16:27 -0500

    Modifique modelos de bases de datos de la app

--- marvyshopmarket/app/models.py
+++ marvyshopmarket/app/models.py
@@ -1,12 +1,14 @@
 from . import db
 
 class Producto(db.Model):
+    __bind_key__ = 'producto'
     __tablename__ = 'productos'
     prod_Id = db.Column(db.Integer, primary_key=True)
     prod_Nombre = db.Column(db.String(100))
     prod_Precio = db.Column(db.Integer)
     prod_Cantidad = db.Column(db.Integer)
     prod_Fecha_cad = db.Column(db.Date)
+    prod_Img = db.Column(db.LargeBinary)
 
 class Images(db.Model):
     __bind_key__ = 'image'  # Vincula este modelo a la base de datos llamada 'image'
