dd5d6d9c1a5ac39fe8a1dba4a59dadebf9a30961
yamid-dev <yhrodriguez1@hotmail.com>
2024-02-22 06:23:54 -0500

    Agregué algunos estilos desde la rama sign-up

---
+++ marvyshopmarket/app/static/js/script.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById('menu-hamburguesa');
+const menuDesplegable = document.getElementById('menu-desplegable');
+const cerrar= document.querySelector('.cerrar')
+
+menu.addEventListener('click', () => {
+    menuDesplegable.style.display = 'flex';
+});
+
+cerrar.addEventListener('click',()=>{
+    menuDesplegable.style.display = 'none'
+})
